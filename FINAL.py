2a46da4dcbf36a0e85f3cf124d95a4674c567cff
Prot0type1 <solorio011@hotmail.com>
2020-12-17 19:20:28 -0800

    structured-english

---
+++ FINAL.py
@@ -0,0 +1,20 @@
+"""
+start
+
+this program will output the grades of students on the final by: 
+    -number of grades
+    -average of grades
+    -percentage of grades above the average percentage
+
+the average grade is 83.25.
+the total number of grades is 24.
+the percentage of grades above average is 54.17
+the list will be introduced from the dictionary by using the open method
+the grades will then be listed as integers.
+after, the calculations can begin.
+the end result will display the 3 solutions.
+
+finalize
+
+"""
+
